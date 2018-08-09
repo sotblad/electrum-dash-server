51681dd209de93e7bc0b41f3d30222d80b80cc15
Sotiris Blad <sotiris2009@hotmail.com>
2018-08-09 18:22:36 +0300

    Update networks.py

--- src/networks.py
+++ src/networks.py
@@ -1,15 +1,15 @@
 # Main network and testnet3 definitions
 
-# Dash src/chainparams.cpp
+# MonetaryUnit src/chainparams.cpp
 params = {
-    'dash_main': {
-        'pubkey_address': 16, #L120
-        'script_address': 76, #L122
+    'mue_main': {
+        'pubkey_address': 76, #L120
+        'script_address': 16, #L122
         'genesis_hash': '000002acd994a815401fbaae0e52404b32857efd0b7b0c77b8e0715ccdd6d437' #L110
     },
-    'dash_test': {
-        'pubkey_address': 140, #L220
-        'script_address': 19, #L222
-        'genesis_hash': '00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c' #L210
+    'mue_test': {
+        'pubkey_address': 19, #L220
+        'script_address': 38, #L222
+        'genesis_hash': '00000c4e1485ee323e0dfab3c8afa871ceceff8822b9abe68005e22576d47a6f' #L210
     }
 }
